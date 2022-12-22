bb7de92af687de6efbf13db69e3ffa07b61ff0e1
Janar <117728309+janaliiv@users.noreply.github.com>
2022-12-22 14:56:54 +0200

    ISSUE-31: add widget configuration options (#58)

---
+++ DSL.Resql/set-widget-config.sql
@@ -0,0 +1,7 @@
+INSERT INTO configuration (key, value, created)
+VALUES ('widgetProactiveSeconds', :widgetProactiveSeconds, :created::timestamp with time zone),
+       ('widgetDisplayBubbleMessageSeconds', :widgetDisplayBubbleMessageSeconds, :created::timestamp with time zone),
+       ('widgetBubbleMessageText', :widgetBubbleMessageText, :created::timestamp with time zone),
+       ('widgetColor', :widgetColor, :created::timestamp with time zone),
+       ('isWidgetActive', :isWidgetActive, :created::timestamp with time zone)
+RETURNING key, value;
