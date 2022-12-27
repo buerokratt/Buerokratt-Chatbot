85b0121e9ed9ca773b3ed1b43f9970424c384494
Heleri Mandla <heleri.mandla@gmail.com>
2022-12-27 17:32:43 +0200

    ISSUE-22 add csa activity status field

--- DSL.Resql/set-customer-support-status.sql
+++ DSL.Resql/set-customer-support-status.sql
@@ -1,2 +1,2 @@
-INSERT INTO customer_support_agent_activity (id_code, active, created)
-VALUES (:userIdCode, :active, :created::timestamp with time zone)
+INSERT INTO customer_support_agent_activity (id_code, active, created, status)
+VALUES (:userIdCode, :active, :created::timestamp with time zone, :status::status)
