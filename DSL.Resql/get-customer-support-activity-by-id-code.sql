069216e85d8c5e0f0473cd06b763ac4004ba98e2
Heleri <heleri.mandla@gmail.com>
2022-12-28 12:42:26 +0200

    issue-22: add admin user status changes (#59)

--- DSL.Resql/get-customer-support-activity-by-id-code.sql
+++ DSL.Resql/get-customer-support-activity-by-id-code.sql
@@ -1,4 +1,4 @@
-SELECT id_code, active
+SELECT id_code, active, status
 FROM customer_support_agent_activity
 WHERE id_code = :customerSupportId
   AND id IN (SELECT max(id) FROM customer_support_agent_activity WHERE id_code = :customerSupportId)
