4d91b588ae945b382c3600f8f6e1950d87bea89c
Heleri <heleri.mandla@gmail.com>
2022-12-30 16:06:42 +0200

    ISSUE-12 return csa status with user data (#60)

--- DSL.Resql/get-users-with-roles-by-role.sql
+++ DSL.Resql/get-users-with-roles-by-role.sql
@@ -5,7 +5,8 @@ SELECT u.login,
        u.display_name,
        u.csa_title,
        u.csa_email,
-       ua.authority_name AS authorities
+       ua.authority_name AS authorities,
+       csa.status AS customerSupportStatus
 FROM "user" u
          LEFT JOIN (SELECT authority_name, user_id
                     FROM user_authority AS ua
@@ -13,6 +14,7 @@ FROM "user" u
                       AND ua.id IN (SELECT max(id)
                                     FROM user_authority
                                     GROUP BY user_id)) ua ON u.id_code = ua.user_id
-WHERE status <> 'deleted'
+         JOIN customer_support_agent_activity as csa on u.id_code = csa.id_code
+WHERE u.status <> 'deleted'
   AND array_length(authority_name, 1) > 0
-  AND id IN (SELECT max(id) FROM "user" GROUP BY id_code);
+  AND u.id IN (SELECT max(id) FROM "user" GROUP BY id_code);
