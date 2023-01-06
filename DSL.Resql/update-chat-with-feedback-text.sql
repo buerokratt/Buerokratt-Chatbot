e382b93159f1c1fea55143864444e3221566004f
Heleri Mandla <heleri.mandla@gmail.com>
2023-01-06 16:37:03 +0200

    ISSUE-9 rename csaTitle fields

--- DSL.Resql/update-chat-with-feedback-text.sql
+++ DSL.Resql/update-chat-with-feedback-text.sql
@@ -19,7 +19,7 @@ SELECT base_id,
        forwarded_to_name,
        received_from,
        received_from_name,
-       csa_title
+       csaTitle
 FROM chat
 WHERE base_id = :id
 ORDER BY updated DESC
