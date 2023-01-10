da8139efe099b0b0ab555505d6770a2aedea06e4
Heleri <heleri.mandla@gmail.com>
2023-01-10 13:56:27 +0200

    ISSUE-46 send authentication success event and rename csaTitle field (#63)

--- DSL.Resql/insert-user.sql
+++ DSL.Resql/insert-user.sql
@@ -1,2 +1,2 @@
 INSERT INTO "user" (login, display_name, password_hash, id_code, status, created, csa_title, csa_email)
-VALUES (:displayName, :displayName, :displayName, :userIdCode, :status, :created::timestamp with time zone, :csa_title, :csa_email);
+VALUES (:displayName, :displayName, :displayName, :userIdCode, :status, :created::timestamp with time zone, :csaTitle, :csa_email);
