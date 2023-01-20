574308fa8a6c23ee1370f662ac601807dd43b58e
Heleri Mandla <heleri.mandla@gmail.com>
2023-01-20 10:26:55 +0200

    ISSUE-39 chat forwarding and takeover info

--- DSL.Resql/insert-message.sql
+++ DSL.Resql/insert-message.sql
@@ -1,4 +1,3 @@
-INSERT INTO message(chat_base_id, base_id, content, event, author_timestamp, author_id, author_first_name, author_last_name, author_role, rating, created, forwarded_from_csa, forwarded_to_csa)
+INSERT INTO message(chat_base_id, base_id, content, event, author_timestamp, author_id, author_first_name, author_last_name, author_role, rating, created, forwarded_by_user, forwarded_from_csa, forwarded_to_csa)
 VALUES (:chatId, :messageId, :content, :event, :authorTimestamp::timestamp with time zone, :authorId, :authorFirstName, :authorLastName,
-        :authorRole, :rating, :created::timestamp with time zone,
-        :forwardedFromCsa,:forwardedToCsa);
+        :authorRole, :rating, :created::timestamp with time zone, :forwardedByUser, :forwardedFromCsa,:forwardedToCsa);
