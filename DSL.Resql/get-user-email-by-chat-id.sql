0d6818e4502291a9a99bd0765ba1e06bf204c3c7
Heleri Mandla <heleri.mandla@gmail.com>
2023-02-02 17:27:26 +0200

    feature #38 refactor

--- DSL.Resql/get-user-email-by-chat-id.sql
+++ DSL.Resql/get-user-email-by-chat-id.sql
@@ -1,5 +1,11 @@
-SELECT u.csa_email
-FROM "user" u
-         INNER JOIN (SELECT customer_support_id, base_id
-                     FROM chat AS c
-                     WHERE c.base_id = :chatId) c ON u.id_code = c.customer_support_id
+SELECT csa_email
+FROM "user"
+    INNER JOIN (SELECT customer_support_id, base_id
+    FROM chat AS c
+    WHERE c.base_id = :chatId) c ON id_code = c.customer_support_id
+WHERE status <> 'deleted'
+AND id IN (SELECT MAX(id) FROM "user" GROUP BY id_code)
+
+
+
+
