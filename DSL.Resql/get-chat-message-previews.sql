b81923af2650ecc3e704ab65eee144c3e9ab872b
Heleri Mandla <heleri.mandla@gmail.com>
2023-02-09 12:02:38 +0200

    feature: #47

--- DSL.Resql/get-chat-message-previews.sql
+++ DSL.Resql/get-chat-message-previews.sql
@@ -1,8 +1,1 @@
-SELECT m.base_id      AS id,
-       m.chat_base_id AS chatId,
-       m.content,
-       m.event,
-       created
-FROM message m
-WHERE id IN (SELECT max(id) FROM message WHERE chat_base_id = :chatId GROUP BY base_id)
-ORDER BY created;
+SELECT * FROM message_preview WHERE chat_base_id = :chatId
--- DSL.Resql/insert-message-preview.sql
+++ DSL.Resql/insert-message-preview.sql
@@ -1,7 +1,3 @@
-INSERT INTO message_preview(chat_base_id, base_id,  content, event, created)
-VALUES (:chatId,
-        (CASE
-             WHEN :messageId IS NOT NULL AND :messageId <> '' THEN :messageId
-             ELSE (gen_random_uuid()::varchar) END),
-        :content, :event,
-        :created::timestamp with time zone);
+INSERT INTO message_preview(chat_base_id, content)
+VALUES (:chatId, :content)
+ON CONFLICT(chat_base_id) DO UPDATE SET content = :content;
