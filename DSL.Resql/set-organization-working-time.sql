104829eca346e1a3ab5b7bbed450f8c2206a9eca
Sergei Rudz <sergei.rudz@we.ee>
2023-02-22 12:33:26 +0200

    Revert "add additional fields for organization working time (#150)"
    This reverts commit 2c197e803f32bded8bd3194e041338ff859bc7ee.

--- DSL.Resql/set-organization-working-time.sql
+++ DSL.Resql/set-organization-working-time.sql
@@ -1,7 +1,5 @@
 INSERT INTO configuration (key, value, created)
 VALUES
     ('organizationWorkingTimeStartISO', :organizationWorkingTimeStartISO, :created::timestamp with time zone),
-    ('organizationWorkingTimeEndISO', :organizationWorkingTimeEndISO, :created::timestamp with time zone),
-    ('organizationWorkingTimeWeekdays', :organizationWorkingTimeWeekdays, :created::timestamp with time zone),
-    ('organizationWorkingTimeHolidays', :organizationWorkingTimeHolidays, :created::timestamp with time zone)
+    ('organizationWorkingTimeEndISO', :organizationWorkingTimeEndISO, :created::timestamp with time zone)
 RETURNING key, value;
