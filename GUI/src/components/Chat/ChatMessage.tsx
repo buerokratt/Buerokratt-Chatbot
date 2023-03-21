9f34edfb0b0c329464e05626b4073a5b032a0fc1
Sergei Rudz <31508949+sergeirudz@users.noreply.github.com>
2023-03-21 13:10:27 +0200

    feat #11 add message read to messages

--- GUI/src/components/Chat/ChatMessage.tsx
+++ GUI/src/components/Chat/ChatMessage.tsx
@@ -14,35 +14,46 @@ const ChatMessage: FC<ChatMessageProps> = ({ message, onSelect }) => {
   const [selected, setSelected] = useState(false);
 
   return (
-    <div
-      className={clsx('active-chat__message', {
-        'active-chat__message--selected': selected,
-      })}
-    >
-      {!!message.preview ? (
-        <div className="active-chat__message-preview">{message.preview}</div>
-      ) : (
-        <div
-          className="active-chat__message-text"
-          onClick={() => {
-            setSelected(!selected);
-            onSelect(message);
-          }}
-        >
-          {message.content}
-        </div>
-      )}
-      <time
-        dateTime={message.authorTimestamp}
-        className="active-chat__message-date"
+    <div className={clsx('active-chat__messageContainer')}>
+      <div
+        className={clsx('active-chat__message', {
+          'active-chat__message--selected': selected,
+        })}
       >
-        {format(new Date(message.authorTimestamp), 'HH:ii:ss')}
-      </time>
-      {selected && (
-        <div className="active-chat__selection-icon">
-          <MdOutlineCheck />
-        </div>
-      )}
+        {!!message.preview ? (
+          <div className="active-chat__message-preview">{message.preview}</div>
+        ) : (
+          <div
+            className="active-chat__message-text"
+            onClick={() => {
+              setSelected(!selected);
+              onSelect(message);
+            }}
+          >
+            {message.content}
+          </div>
+        )}
+        <time
+          dateTime={message.authorTimestamp}
+          className="active-chat__message-date"
+        >
+          {format(new Date(message.authorTimestamp), 'HH:ii:ss')}
+        </time>
+        {selected && (
+          <div className="active-chat__selection-icon">
+            <MdOutlineCheck />
+          </div>
+        )}
+      </div>
+      {message.event === 'message-read' ? (
+        <span className="active-chat__message-status">
+          Loetud
+          <time dateTime={message.authorTimestamp}>
+            {' '}
+            {format(new Date(message.authorTimestamp), 'HH:ii:ss')}
+          </time>
+        </span>
+      ) : null}{' '}
     </div>
   );
 };
