99afe617a85ecad7d87500c2998c386b4f13ef4f
ahmedyasser <ahmedyasser.dev@hotmail.com>
2023-08-08 03:49:15 +0300

    Fixed Bot Appearance Color

--- GUI/src/components/FormElements/FormInput/index.tsx
+++ GUI/src/components/FormElements/FormInput/index.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, InputHTMLAttributes, useId } from 'react';
+import { forwardRef, InputHTMLAttributes, PropsWithChildren, useId } from 'react';
 import clsx from 'clsx';
 import { MdOutlinePalette } from 'react-icons/md';
 
@@ -6,17 +6,16 @@ import { Icon } from 'components';
 import './FormInput.scss';
 import { CHAT_INPUT_LENGTH } from 'constants/config';
 
-type InputProps = InputHTMLAttributes<HTMLInputElement> & {
+type InputProps = PropsWithChildren<InputHTMLAttributes<HTMLInputElement>> & {
   label: string;
   name: string;
   hideLabel?: boolean;
-  colorInput?: boolean;
   maxLength?: number;
 };
 
 const FieldInput = forwardRef<HTMLInputElement, InputProps>(
   (
-    { label, name, disabled, hideLabel, colorInput, maxLength, ...rest },
+    { label, name, disabled, hideLabel, maxLength, children, ...rest },
     ref
   ) => {
     const id = useId();
@@ -38,14 +37,9 @@ const FieldInput = forwardRef<HTMLInputElement, InputProps>(
             id={id}
             ref={ref}
             aria-label={hideLabel ? label : undefined}
-            pattern={colorInput ? '^#([a-fA-F0-9]{3}){1,2}$' : undefined}
             {...rest}
           />
-          {colorInput && (
-            <Icon
-              icon={<MdOutlinePalette fontSize={20} color="rgba(0,0,0,0.54)" />}
-            />
-          )}
+          {children}
         </div>
       </div>
     );
--- GUI/src/pages/Settings/SettingsAppearance/index.tsx
+++ GUI/src/pages/Settings/SettingsAppearance/index.tsx
@@ -1,17 +1,27 @@
-import { FC, useRef, useState } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { useTranslation } from 'react-i18next';
-import { useForm, Controller, useWatch } from 'react-hook-form';
+import { useForm, Controller, useWatch, useFormContext } from 'react-hook-form';
 import { motion } from 'framer-motion';
 import { AxiosError } from 'axios';
 
-import { Button, Card, FormInput, FormSelect, Switch, Track } from 'components';
+import {
+  Button,
+  Card,
+  FormInput,
+  FormSelect,
+  Icon,
+  Switch,
+  Track,
+} from 'components';
 import { WidgetConfig } from 'types/widgetConfig';
 import { useToast } from 'hooks/useToast';
 import bykLogo from 'assets/logo-white.svg';
 import apiDevV2 from 'services/api-dev-v2';
 import './SettingsAppearance.scss';
 import clsx from 'clsx';
+import { ChromePicker } from 'react-color';
+import { MdOutlinePalette } from 'react-icons/md';
 
 const variants = {
   initial: {
@@ -26,8 +36,10 @@ const SettingsAppearance: FC = () => {
   const { t } = useTranslation();
   const toast = useToast();
   const hasRendered = useRef<boolean>();
-  const { register, control, handleSubmit, reset } = useForm<WidgetConfig>();
+  const { register, control, handleSubmit, reset, setValue } =
+    useForm<WidgetConfig>();
   const [showPreview, setShowPreview] = useState(false);
+  const [showColorPalette, setShowColorPalette] = useState(false);
   const [delayFinished, setDelayFinished] = useState(false);
   const { data: widgetConfig } = useQuery<WidgetConfig>({
     queryKey: ['cs-get-widget-config', 'prod-2'],
@@ -45,14 +57,31 @@ const SettingsAppearance: FC = () => {
     },
   });
 
-  const widgetProactiveSeconds = useWatch({ control, name: 'widgetProactiveSeconds' });
-  const widgetBubbleMessageText = useWatch({ control, name: 'widgetBubbleMessageText' });
-  const widgetDisplayBubbleMessageSeconds = useWatch({ control, name: 'widgetDisplayBubbleMessageSeconds' });
+  const widgetProactiveSeconds = useWatch({
+    control,
+    name: 'widgetProactiveSeconds',
+  });
+  const widgetBubbleMessageText = useWatch({
+    control,
+    name: 'widgetBubbleMessageText',
+  });
+  const widgetDisplayBubbleMessageSeconds = useWatch({
+    control,
+    name: 'widgetDisplayBubbleMessageSeconds',
+  });
   const widgetColor = useWatch({ control, name: 'widgetColor' });
   const widgetAnimation = useWatch({ control, name: 'widgetAnimation' });
 
   const widgetConfigMutation = useMutation({
-    mutationFn: (data: WidgetConfig) => apiDevV2.post<WidgetConfig>('cs-set-widget-config', data),
+    mutationFn: (data: WidgetConfig) =>
+      apiDevV2.post<WidgetConfig>('cs-set-widget-config', data),
+    onSuccess: () => {
+      toast.open({
+        type: 'success',
+        title: t('global.notification'),
+        message: t('toast.success.updateSuccess'),
+      });
+    },
     onError: (error: AxiosError) => {
       toast.open({
         type: 'error',
@@ -61,6 +90,28 @@ const SettingsAppearance: FC = () => {
       });
     },
   });
+
+  const colorComponentRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const handleDocumentClick = (event: MouseEvent) => {
+      handleClickOutside(event);
+    };
+    document.addEventListener('click', handleDocumentClick);
+
+    return () => {
+      document.removeEventListener('click', handleDocumentClick);
+    };
+  }, []);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      colorComponentRef.current &&
+      !colorComponentRef.current.contains(event.target as Node)
+    ) {
+      setShowColorPalette(false);
+    }
+  };
 
   const handleFormSubmit = handleSubmit((data) => {
     widgetConfigMutation.mutate(data);
@@ -84,39 +135,41 @@ const SettingsAppearance: FC = () => {
   if (!widgetConfig) return <>Loading...</>;
 
   return (
-    <>
+    <div ref={colorComponentRef}>
       <h1>{t('settings.appearance.title')}</h1>
 
       <Card
         footer={
-          <Track gap={8} justify='end'>
+          <Track gap={8} justify="end">
             <Button onClick={handleFormSubmit}>{t('global.save')}</Button>
-            <Button appearance='secondary' onClick={handlePreview}>{t('global.preview')}</Button>
+            <Button appearance="secondary" onClick={handlePreview}>
+              {t('global.preview')}
+            </Button>
           </Track>
         }
       >
-        <Track gap={8} direction='vertical' align='left'>
+        <Track gap={8} direction="vertical" align="left">
           <FormInput
             {...register('widgetProactiveSeconds')}
             label={t('settings.appearance.widgetProactiveSeconds')}
-            type='number'
+            type="number"
           />
           <Controller
-            name='isWidgetActive'
+            name="isWidgetActive"
             control={control}
-            render={({ field }) =>
+            render={({ field }) => (
               <Switch
                 onCheckedChange={field.onChange}
                 label={t('settings.appearance.widgetBubbleMessageText')}
                 checked={field.value}
                 {...field}
               />
-            }
+            )}
           />
           <FormInput
             {...register('widgetDisplayBubbleMessageSeconds')}
             label={t('settings.appearance.widgetDisplayBubbleMessageSeconds')}
-            type='number'
+            type="number"
           />
           <FormInput
             {...register('widgetBubbleMessageText')}
@@ -124,16 +177,42 @@ const SettingsAppearance: FC = () => {
           />
           <FormInput
             {...register('widgetColor')}
+            readOnly={true}
             label={t('settings.appearance.widgetColor')}
-            colorInput
-          />
+            onClick={() => setShowColorPalette(!showColorPalette)}
+          >
+            {
+              <div>
+                <Icon
+                  icon={
+                    <MdOutlinePalette fontSize={20} color="rgba(0,0,0,0.54)" />
+                  }
+                  onIconClicked={() => setShowColorPalette(!showColorPalette)}
+                />
+                {showColorPalette && (
+                  <div style={{ position: 'absolute', zIndex: '2' }}>
+                    <div
+                      onClick={() => setShowColorPalette(!showColorPalette)}
+                    />
+                    <ChromePicker
+                      {...register('widgetColor')}
+                      color={widgetColor}
+                      onChange={(color) => setValue('widgetColor', color.hex)}
+                    />
+                  </div>
+                )}
+              </div>
+            }
+          </FormInput>
           <Controller
-            name='widgetAnimation'
+            name="widgetAnimation"
             control={control}
             render={({ field }) => (
               <FormSelect
                 {...field}
-                onSelectionChange={(selection) => field.onChange(selection?.value)}
+                onSelectionChange={(selection) =>
+                  field.onChange(selection?.value)
+                }
                 label={t('settings.appearance.widgetAnimation')}
                 defaultValue={field.value}
                 options={[
@@ -142,36 +221,44 @@ const SettingsAppearance: FC = () => {
                   { label: 'Wiggle', value: 'wiggle' },
                 ]}
               />
-            )} />
+            )}
+          />
         </Track>
       </Card>
 
       {showPreview && (
-        <div className='profile__wrapper'>
+        <div className="profile__wrapper">
           <motion.div
-            className={clsx('profile', delayFinished && {
-              'profile--shockwave': widgetAnimation === 'shockwave',
-              'profile--jump': widgetAnimation === 'jump',
-              'profile--wiggle': widgetAnimation === 'wiggle',
-            })}
+            className={clsx(
+              'profile',
+              delayFinished && {
+                'profile--shockwave': widgetAnimation === 'shockwave',
+                'profile--jump': widgetAnimation === 'jump',
+                'profile--wiggle': widgetAnimation === 'wiggle',
+              }
+            )}
             variants={variants}
-            initial='initial'
-            animate='animate'
+            initial="initial"
+            animate="animate"
             style={{
               animationIterationCount: widgetProactiveSeconds,
               backgroundColor: widgetColor,
             }}
           >
-            <img src={bykLogo} alt='Buerokratt logo' width={45} />
+            <img src={bykLogo} alt="Buerokratt logo" width={45} />
           </motion.div>
-          {control._formValues.isWidgetActive && 
-            <div className={clsx('profile__greeting-message', { 'profile__greeting-message--active': delayFinished })}>
+          {control._formValues.isWidgetActive && (
+            <div
+              className={clsx('profile__greeting-message', {
+                'profile__greeting-message--active': delayFinished,
+              })}
+            >
               {widgetBubbleMessageText}
             </div>
-          }
+          )}
         </div>
       )}
-    </>
+    </div>
   );
 };
 
