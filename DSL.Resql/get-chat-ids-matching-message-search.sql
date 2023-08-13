8778df41247329fd6f32a02703efc4d89bae0dd3
ahmedyasser <ahmedyasser.dev@hotmail.com>
2023-08-13 13:16:34 +0300

    Added Search by user info & csa display name

--- DSL.Resql/get-chat-ids-matching-message-search.sql
+++ DSL.Resql/get-chat-ids-matching-message-search.sql
@@ -4,11 +4,21 @@ left join chat on message.chat_base_id = chat.base_id left join chat_history_com
 WHERE lower(content) LIKE
       (SELECT lower(regexp_replace(regexp_replace(:searchKey, E' ', E'%', 'g'), E'(.*)', E'%\\1%', 'g')))
 OR lower(chat_base_id) LIKE
-      (SELECT lower(regexp_replace(regexp_replace(:searchKey, E' ', E'%', 'g'), E'(.*)', E'%\\1%', 'g')))      
-OR lower(message.author_first_name) LIKE
+      (SELECT lower(regexp_replace(regexp_replace(:searchKey, E' ', E'%', 'g'), E'(.*)', E'%\\1%', 'g')))       
+OR lower(chat.customer_support_display_name) LIKE
+      (SELECT lower(regexp_replace(regexp_replace(:searchKey, E' ', E'%', 'g'), E'(.*)', E'%\\1%', 'g'))) 
+OR lower(chat.end_user_first_name) LIKE
       (SELECT lower(regexp_replace(regexp_replace(:searchKey, E' ', E'%', 'g'), E'(.*)', E'%\\1%', 'g')))
-OR lower(message.author_last_name) LIKE
-      (SELECT lower(regexp_replace(regexp_replace(:searchKey, E' ', E'%', 'g'), E'(.*)', E'%\\1%', 'g')))   
+OR lower(chat.end_user_last_name) LIKE
+      (SELECT lower(regexp_replace(regexp_replace(:searchKey, E' ', E'%', 'g'), E'(.*)', E'%\\1%', 'g')))
+OR lower(chat.end_user_id) LIKE
+      (SELECT lower(regexp_replace(regexp_replace(:searchKey, E' ', E'%', 'g'), E'(.*)', E'%\\1%', 'g')))
+OR lower(chat.end_user_email) LIKE
+      (SELECT lower(regexp_replace(regexp_replace(:searchKey, E' ', E'%', 'g'), E'(.*)', E'%\\1%', 'g')))
+OR lower(chat.end_user_phone) LIKE
+      (SELECT lower(regexp_replace(regexp_replace(:searchKey, E' ', E'%', 'g'), E'(.*)', E'%\\1%', 'g')))
+OR lower(chat.end_user_url) LIKE
+      (SELECT lower(regexp_replace(regexp_replace(:searchKey, E' ', E'%', 'g'), E'(.*)', E'%\\1%', 'g')))       
 OR lower(chat_history_comments.comment) LIKE
       (SELECT lower(regexp_replace(regexp_replace(:searchKey, E' ', E'%', 'g'), E'(.*)', E'%\\1%', 'g')))   
 GROUP BY chat_base_id;
