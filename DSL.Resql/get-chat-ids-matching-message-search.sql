63e8ccb893b20cd7658162dff161e55b8a1a9b32
ahmedyasser <ahmedyasser.dev@hotmail.com>
2023-08-13 12:52:15 +0300

    Fixed Search by chat id

--- DSL.Resql/get-chat-ids-matching-message-search.sql
+++ DSL.Resql/get-chat-ids-matching-message-search.sql
@@ -2,4 +2,10 @@ SELECT chat_base_id as chat_id
 FROM message
 WHERE lower(content) LIKE
       (SELECT lower(regexp_replace(regexp_replace(:searchKey, E' ', E'%', 'g'), E'(.*)', E'%\\1%', 'g')))
+OR lower(chat_base_id) LIKE
+      (SELECT lower(regexp_replace(regexp_replace(:searchKey, E' ', E'%', 'g'), E'(.*)', E'%\\1%', 'g')))      
+OR lower(author_first_name) LIKE
+      (SELECT lower(regexp_replace(regexp_replace(:searchKey, E' ', E'%', 'g'), E'(.*)', E'%\\1%', 'g')))
+OR lower(author_last_name) LIKE
+      (SELECT lower(regexp_replace(regexp_replace(:searchKey, E' ', E'%', 'g'), E'(.*)', E'%\\1%', 'g')))     
 GROUP BY chat_base_id;
