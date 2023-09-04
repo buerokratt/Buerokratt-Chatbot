56223ac7cca00946ca808f6f55595880d7b77f73
Ahmed yasser <ahmedyasser.dev@hotmail.com>
2023-09-04 15:52:23 +0300

    Bug Fix User Settings (#286)
    * Linked User Profile Settings
    
    * Added Fixed to ruuter & user profile settings

--- GUI/src/components/Header/index.tsx
+++ GUI/src/components/Header/index.tsx
@@ -68,7 +68,7 @@ const Header: FC = () => {
   const [userProfileSettings, setUserProfileSettings] =
     useState<UserProfileSettings>({
       userId: 1,
-      forwardedChatPopupNotifications: false,
+      forwardedChatPopupNotifications: true,
       forwardedChatSoundNotifications: true,
       forwardedChatEmailNotifications: false,
       newChatPopupNotifications: false,
@@ -79,16 +79,15 @@ const Header: FC = () => {
 
   useEffect(() => {
     getMessages();
-  }, []);
+  }, [userInfo?.idCode]);
 
   const getMessages = async () => {
-    const { data: res } = await apiDevV2.get('cs-get-user-profile-settings', {
-      params: {
-        // TODO: Use actual id from userInfo once it starts using real data
-        userId: 1,
-      },
+    const { data: res } = await apiDevV2.post('cs-get-user-profile-settings', {
+      userId: userInfo?.idCode ?? '',
     });
-    if (res.response) setUserProfileSettings(res.response);
+
+    if (res.response && res.response != 'error: not found')
+      setUserProfileSettings(res.response[0]);
   };
   const { data: customerSupportActivity } = useQuery<CustomerSupportActivity>({
     queryKey: ['cs-get-customer-support-activity', 'prod'],
@@ -190,7 +189,16 @@ const Header: FC = () => {
 
   const userProfileSettingsMutation = useMutation({
     mutationFn: async (data: UserProfileSettings) => {
-      await apiDevV2.post('cs-set-user-profile-settings', data);
+      await apiDevV2.post('cs-set-user-profile-settings', {
+        userId: userInfo?.idCode ?? '',
+        forwardedChatPopupNotifications: data.forwardedChatPopupNotifications,
+        forwardedChatSoundNotifications: data.forwardedChatSoundNotifications,
+        forwardedChatEmailNotifications: data.newChatEmailNotifications,
+        newChatPopupNotifications: data.newChatPopupNotifications,
+        newChatSoundNotifications: data.newChatSoundNotifications,
+        newChatEmailNotifications: data.newChatEmailNotifications,
+        useAutocorrect: data.useAutocorrect,
+      });
       setUserProfileSettings(data);
     },
     onError: async (error: AxiosError) => {
@@ -239,9 +247,7 @@ const Header: FC = () => {
       if (data.custom_jwt_extend === null) return;
       setNewCookie(data.custom_jwt_extend);
     },
-    onError: (error: AxiosError) => {
-      console.log('E: ', error);
-    },
+    onError: (error: AxiosError) => {},
   });
 
   const logoutMutation = useMutation({
