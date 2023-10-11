84fb3eb8dfd27f43c07b57b8cde9ff87cb77338c
Varmo <101868197+varmoh@users.noreply.github.com>
2023-10-11 09:58:26 +0300

    Create env-config.js

---
+++ setup/customer-support/env-config.js
@@ -0,0 +1,8 @@
+window._env_ = {
+  RUUTER_API_URL: 'https://ruuter..buerokratt.ee/priv1',
+  TIM_API_URL: 'https://tim..buerokratt.ee',
+  TARA_REDIRECT_URL: 'https://tim..buerokratt.ee/oauth2/authorization/tara?callback_url=https://admin..buerokratt.ee/auth/callback',
+  MONITORING_URL: 'http://localhost:3003',
+  BACKOFFICE_URL: 'https://admin..buerokratt.ee/chat',
+  PASSWORD_AUTH_ENABLED: false,
+};
