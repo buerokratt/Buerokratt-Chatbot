d9dc3cbe973cb0f56a62db0f8a614dbdd650e9b2
baha-a <bhlshrf@gmail.com>
2023-11-01 10:51:47 +0300

    Improve Server-sent events implementation

--- GUI/src/services/sse-service.ts
+++ GUI/src/services/sse-service.ts
@@ -2,35 +2,24 @@ import { RuuterResponse } from '../model/ruuter-response-model';
 
 const ruuterUrl = import.meta.env.REACT_APP_RUUTER_V1_PRIVATE_API_URL;
 
-interface SseInstance {
-  onMessage: <T>(handleData: (data: T) => void) => void;
-  close: () => void;
-}
-
-const sse = (url: string): SseInstance => {
+const sse = <T>(url: string, onMessage: (data: T) => void): EventSource => {
   const eventSource = new EventSource(`${ruuterUrl}/sse/${url}`, { withCredentials: true });
 
-  const onMessage = <T>(handleData: (data: T) => void) => {
-    eventSource.onmessage = (event: MessageEvent) => {
-      const response = JSON.parse(event.data);
+  eventSource.onmessage = (event: MessageEvent) => {
+    const response = JSON.parse(event.data);
 
-      if (response.statusCodeValue === 200) {
-        const ruuterResponse = response.body as RuuterResponse;
-        if (ruuterResponse === null) return;
-        if (ruuterResponse.data) handleData(Object.values(ruuterResponse.data)[0] as T);
-      }
-    };
-  };
-
-  const close = () => {
-    eventSource.close();
+    if (response.statusCodeValue === 200) {
+      const ruuterResponse = response.body as RuuterResponse;
+      if (ruuterResponse?.data)
+        onMessage(Object.values(ruuterResponse.data)[0] as T);
+    }
   };
 
   eventSource.onerror = () => {
-    eventSource.close();
+    console.error('SSE error');
   };
 
-  return { onMessage, close };
+  return eventSource;
 };
 
 export default sse;
