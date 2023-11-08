f9375ace04912f840c54b2164770caa39f271c0d
ExiRain <vassilimoskaljov@gmail.com>
2023-11-08 11:37:40 +0200

    Added changes to Layout to use menu from package and header from components

--- GUI/src/components/Layout/index.tsx
+++ GUI/src/components/Layout/index.tsx
@@ -1,21 +1,49 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { Outlet } from 'react-router-dom';
-
-import { MainNavigation, Header } from 'components';
+import  Header from '../Header';
+import {
+    MainNavigation
+} from '@exirain/header/src';
 import './Layout.scss';
+import {useQuery} from "@tanstack/react-query";
+import imts from './menu-structure.json';
 
-const Layout: FC = () => {
-  return (
-    <div className='layout'>
-      <MainNavigation />
-      <div className='layout__wrapper'>
-        <Header />
-        <main className='layout__main'>
-          <Outlet />
-        </main>
-      </div>
-    </div>
-  );
-};
+const Layout: FC = () => {   const CACHE_NAME = 'mainmenu-cache';
+
+
+    const [MainMenuItems, setMainMenuItems] = useState(imts)
+
+    const  {data, isLoading, status}  = useQuery({
+        queryKey: [import.meta.env.REACT_APP_MENU_URL + import.meta.env.REACT_APP_MENU_PATH],
+        onSuccess: (res: any) => {
+            try {
+                setMainMenuItems(res);
+                localStorage.setItem(CACHE_NAME, JSON.stringify(itms));
+            } catch (e) {
+                console.log(e);
+            }
+        },
+        onError: (error: any) => {
+            setMainMenuItems(getCache());
+        }
+
+    });
+
+    function getCache(): any {
+        const cache = localStorage.getItem(CACHE_NAME) || '{}';
+        return JSON.parse(cache);
+    }
+
+    return (
+        <div className="layout">
+            <MainNavigation serviceId={import.meta.env.REACT_APP_SERVICE_ID.split(',')} items={MainMenuItems}/>
+            <div className="layout__wrapper">
+                <Header />
+                <main className="layout__main">
+                    <Outlet />
+                </main>
+            </div>
+        </div>
+    )};
 
 export default Layout;
