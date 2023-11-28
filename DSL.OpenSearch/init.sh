57db7b53551dd0e50af580256ffc9a166f891476
baha-a <bhlshrf@gmail.com>
2023-11-28 18:37:18 +0300

    Seed Opensearch with mock data

--- DSL.OpenSearch/init.sh
+++ DSL.OpenSearch/init.sh
@@ -1,7 +1,7 @@
 #clear
 curl -XDELETE 'http://localhost:9200/*' -u admin:admin --insecure
 
-#Entities
+#SSE events
 curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/sse-events" -ku admin:admin --data-binary "@fieldMappings/sse-events.json"
 curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/sse-events/_bulk" -ku admin:admin --data-binary "@mock/sse-events.json"
 curl -L -X POST 'http://localhost:9200/_scripts/sse-events-by-type' -H 'Content-Type: application/json' -H 'Cookie: customJwtCookie=test' --data-binary "@templates/sse-events-by-type.json"
