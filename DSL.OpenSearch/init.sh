481f12e2b28a3f0d4ce91bd304faf3126108b746
baha-a <bhlshrf@gmail.com>
2023-11-30 15:45:15 +0300

    Update opensearch index

--- DSL.OpenSearch/init.sh
+++ DSL.OpenSearch/init.sh
@@ -1,6 +1,6 @@
 #clear
 curl -XDELETE 'http://localhost:9200/*' -u admin:admin --insecure
 
-#SSE events
+#notifications
 curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/notifications" -ku admin:admin --data-binary "@fieldMappings/notifications.json"
 curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/notifications/_bulk" -ku admin:admin --data-binary "@mock/notifications.json"
