a16181050374082b7d0f28cae3fe02dbcef7caf4
baha-a <bhlshrf@gmail.com>
2023-11-30 10:27:59 +0300

    Rename opensearch index

--- DSL.OpenSearch/init.sh
+++ DSL.OpenSearch/init.sh
@@ -2,6 +2,5 @@
 curl -XDELETE 'http://localhost:9200/*' -u admin:admin --insecure
 
 #SSE events
-curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/sse-events" -ku admin:admin --data-binary "@fieldMappings/sse-events.json"
-curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/sse-events/_bulk" -ku admin:admin --data-binary "@mock/sse-events.json"
-curl -L -X POST 'http://localhost:9200/_scripts/sse-events-by-type' -H 'Content-Type: application/json' -H 'Cookie: customJwtCookie=test' --data-binary "@templates/sse-events-by-type.json"
+curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/notifications" -ku admin:admin --data-binary "@fieldMappings/notifications.json"
+curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/notifications/_bulk" -ku admin:admin --data-binary "@mock/notifications.json"
