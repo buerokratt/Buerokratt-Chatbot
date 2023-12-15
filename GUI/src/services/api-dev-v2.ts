c01d80389739de2aabea8f1a5f379a7b5aacfacc
Bartek <bartek.codes@gmail.com>
2023-12-15 10:56:05 +0100

    fix: missing env variable

--- GUI/src/services/api-dev-v2.ts
+++ GUI/src/services/api-dev-v2.ts
@@ -10,11 +10,27 @@ const instance = axios.create({
 });
 
 instance.interceptors.response.use(
-  (response) => {
-    return response;
+  (axiosResponse) => {
+    import.meta.env.DEBUG_ENABLED && console.log(axiosResponse);
+    return axiosResponse;
   },
   (error: AxiosError) => {
+    import.meta.env.DEBUG_ENABLED && console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+instance.interceptors.request.use(
+  (axiosRequest) => {
+    import.meta.env.DEBUG_ENABLED && console.log(axiosRequest);
+    return axiosRequest;
+  },
+  (error: AxiosError) => {
+    import.meta.env.DEBUG_ENABLED && console.log(error);
     if (error.response?.status === 401) {
+      //TODO: handle unauthorized requests
+    }
+    if (error.response?.status === 403) {
       //TODO: handle unauthorized requests
     }
     return Promise.reject(error);
