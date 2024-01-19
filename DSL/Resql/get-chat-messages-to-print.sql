17dc62fcacb909ed5a6b028665e396ffcf4cc5d0
baha-a <bhlshrf@gmail.com>
2024-01-19 18:38:36 +0300

    Remove unused SQL columns from Select

--- DSL/Resql/get-chat-messages-to-print.sql
+++ DSL/Resql/get-chat-messages-to-print.sql
@@ -1,20 +1,11 @@
-SELECT m.base_id      AS id,
-       m.chat_base_id AS chat_id,
-       m.content,
-       m.buttons,
+SELECT m.content,
        m.event,
-       m.author_id,
-       m.author_timestamp,
+       m.created,
+       m.author_role,
        m.author_first_name,
        m.author_last_name,
-       m.author_role,
-       m.forwarded_by_user,
-       m.forwarded_from_csa,
-       m.forwarded_to_csa,
        u.csa_title,
-       rating,
-       created,
-       updated
+       m.buttons
 FROM message m
 LEFT JOIN "user" u ON m.author_id = u.id_code
 WHERE m.id IN (SELECT max(id) FROM message WHERE chat_base_id = :chatId GROUP BY base_id)
