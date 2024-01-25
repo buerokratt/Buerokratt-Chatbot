21bc881c9efb4ac748e4bd6dcc04eae925a31e3c
ahmedyasser <ahmedyasser.dev@hotmail.com>
2024-01-25 06:13:50 +0200

    Modified Liquibase int cast

--- DSL/Liquibase/changelog/20231116129861-change_message_rating_type.sql
+++ DSL/Liquibase/changelog/20231116129861-change_message_rating_type.sql
@@ -1,3 +1,3 @@
 -- liquibase formatted sql
 -- changeset ahmedyasser:20231116129861
-ALTER TABLE message ALTER COLUMN rating TYPE INT USING NULLIF(rating, '0')::integer;
+ALTER TABLE message ALTER COLUMN rating TYPE INT USING NULLIF(rating, '')::integer;
--- DSL/Liquibase/changelog/20231116129973-change_chat_feedback_rating_type.sql
+++ DSL/Liquibase/changelog/20231116129973-change_chat_feedback_rating_type.sql
@@ -1,3 +1,3 @@
 -- liquibase formatted sql
 -- changeset ahmedyasser:20231116129973
-ALTER TABLE chat ALTER COLUMN feedback_rating TYPE INT USING NULLIF(feedback_rating, '0')::integer;
+ALTER TABLE chat ALTER COLUMN feedback_rating TYPE INT USING NULLIF(feedback_rating, '')::integer;
