bc2ebcdd219e1f3de2423238455f957650ef3e56
ahmedyasser <ahmedyasser.dev@hotmail.com>
2024-01-25 11:19:41 +0200

    Modified Liquibase

--- DSL/Liquibase/changelog/20231116129861-change_message_rating_type.sql
+++ DSL/Liquibase/changelog/20231116129861-change_message_rating_type.sql
@@ -1,3 +1,3 @@
 -- liquibase formatted sql
 -- changeset ahmedyasser:20231116129861
-ALTER TABLE message ALTER COLUMN rating TYPE INT USING NULLIF(rating, '')::integer;
+ALTER TABLE message ALTER COLUMN rating TYPE INT USING CASE WHEN rating ~ '^\d+$' THEN rating::INTEGER ELSE NULL END;
