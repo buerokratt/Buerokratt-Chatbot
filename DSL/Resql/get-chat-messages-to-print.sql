4da588336c6ffaad88ff62891101870c0f7e3bd0
Bahaa Alsharif <bhlshrf@gmail.com>
2024-02-02 15:53:29 +0300

    Refactor get-chat-messages-to-print.sql query to include user display name (#434)

--- DSL/Resql/get-chat-messages-to-print.sql
+++ DSL/Resql/get-chat-messages-to-print.sql
@@ -2,12 +2,12 @@ SELECT m.content,
        m.event,
        m.created,
        m.author_role,
-       m.author_first_name,
-       m.author_last_name,
+       COALESCE(u.first_name, m.author_first_name, u.display_name) AS author_first_name,
+       COALESCE(u.last_name, m.author_last_name) AS author_last_name,
        u.csa_title,
        m.buttons
 FROM message m
 LEFT JOIN "user" u ON m.author_id = u.id_code
-WHERE m.id IN (SELECT max(id) FROM message WHERE chat_base_id = :chatId GROUP BY base_id)
+WHERE m.chat_base_id = :chatId
   AND m.content != 'message-read'
-ORDER BY m.created;
+ORDER BY m.id;
