2d8f7e2202ad36277792cfa00baa2336c0a507a3
baha-a <bhlshrf@gmail.com>
2024-02-05 18:51:04 +0100

    Fix notification node URL error

--- GUI/src/services/sse-service.ts
+++ GUI/src/services/sse-service.ts
@@ -1,6 +1,10 @@
 const notificationNodeUrl = import.meta.env.REACT_APP_NOTIFICATION_NODE_URL;
 
 const sse = <T>(url: string, onMessage: (data: T) => void): EventSource => {
+  if(!notificationNodeUrl) {
+    console.error('Notification node url is not defined')
+    throw new Error('Notification node url is not defined');
+  }
   const eventSource = new EventSource(`${notificationNodeUrl}/sse/notifications${url}`);
 
   eventSource.onmessage = (event: MessageEvent) => {
