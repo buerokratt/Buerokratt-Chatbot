a78997a2065e3359808c632c7a9610b15807439f
ahmedyasser <26207361+1AhmedYasser@users.noreply.github.com>
2024-05-06 02:35:51 +0300

    Fixed Sonar Issues

--- DSL/Resql/get-chat-ids-matching-message-search.sql
+++ DSL/Resql/get-chat-ids-matching-message-search.sql
@@ -7,23 +7,24 @@ WITH consts AS (
 ), regx AS (
   SELECT lower(regexp_replace(regexp_replace(:searchKey, consts.regxE1, consts.regxE2, consts.regxE3), consts.regxE4, consts.regxE5, consts.regxE3)) AS search_key
   FROM consts
+), filtered_messages AS (
+  SELECT message.chat_base_id AS chat_id
+  FROM message
+  LEFT JOIN chat ON message.chat_base_id = chat.base_id
+  LEFT JOIN chat_history_comments ON message.chat_base_id = chat_history_comments.chat_id
+  JOIN regx ON (
+      lower(content) LIKE regx.search_key
+      OR lower(chat_base_id) LIKE regx.search_key
+      OR lower(chat.customer_support_display_name) LIKE regx.search_key
+      OR lower(chat.end_user_first_name) LIKE regx.search_key
+      OR lower(chat.end_user_last_name) LIKE regx.search_key
+      OR lower(chat.end_user_id) LIKE regx.search_key
+      OR lower(chat.end_user_email) LIKE regx.search_key
+      OR lower(chat.end_user_phone) LIKE regx.search_key
+      OR lower(chat.end_user_url) LIKE regx.search_key
+      OR lower(chat_history_comments.comment) LIKE regx.search_key
+  )
 )
-SELECT chat_base_id AS chat_id
-FROM message
-LEFT JOIN chat ON message.chat_base_id = chat.base_id
-LEFT JOIN chat_history_comments ON message.chat_base_id = chat_history_comments.chat_id 
-WHERE EXISTS (
-  SELECT 1
-  FROM regx
-  WHERE lower(content) LIKE regx.search_key
-     OR lower(chat_base_id) LIKE regx.search_key     
-     OR lower(chat.customer_support_display_name) LIKE regx.search_key
-     OR lower(chat.end_user_first_name) LIKE regx.search_key
-     OR lower(chat.end_user_last_name) LIKE regx.search_key
-     OR lower(chat.end_user_id) LIKE regx.search_key
-     OR lower(chat.end_user_email) LIKE regx.search_key
-     OR lower(chat.end_user_phone) LIKE regx.search_key
-     OR lower(chat.end_user_url) LIKE regx.search_key      
-     OR lower(chat_history_comments.comment) LIKE regx.search_key
-)
-GROUP BY chat_base_id;
+SELECT chat_id
+FROM filtered_messages
+GROUP BY chat_id;
