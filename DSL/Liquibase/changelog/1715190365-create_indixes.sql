d5e7c94c9551874ef7cc448cbfbd02b0a74ca3ac
baha-a <bhlshrf@gmail.com>
2024-05-09 23:48:20 +0300

    Rewrite sql query

--- DSL/Liquibase/changelog/1715190365-create_indixes.sql
+++ DSL/Liquibase/changelog/1715190365-create_indixes.sql
@@ -5,4 +5,3 @@ CREATE INDEX idx_configuration_key ON configuration (key);
 CREATE INDEX idx_chat_base_id ON chat (base_id);
 CREATE INDEX idx_message_chat_base_id ON message (chat_base_id);
 CREATE INDEX idx_message_event ON message (event);
-
