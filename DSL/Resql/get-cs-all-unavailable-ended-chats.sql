6fe0ab7799f1728a27328bed9080feda9b232ef4
Bahaa Alsharif <bhlshrf@gmail.com>
2024-05-13 11:25:39 +0300

    Refactor `get-cs-all-unavailable-ended-chats.sql` Postgres query (#575)
    * Add limit
    
    * Improve readability
    
    * Improve query

--- DSL/Resql/get-cs-all-unavailable-ended-chats.sql
+++ DSL/Resql/get-cs-all-unavailable-ended-chats.sql
@@ -1,8 +1,74 @@
+WITH TitleVisibility AS (
+  SELECT value
+  FROM configuration
+  WHERE NOT deleted AND key = 'is_csa_title_visible'
+  ORDER BY id DESC
+  LIMIT 1
+),
+FulfilledMessages AS (
+  SELECT MAX(id) maxId
+  FROM message
+  WHERE event = 'contact-information-fulfilled'
+  GROUP BY chat_base_id
+),
+ContactsMessage AS (
+  SELECT chat_base_id, content
+  FROM message
+  JOIN FulfilledMessages ON id = maxId
+),
+MaxChats AS (
+  SELECT MAX(id) maxId
+  FROM chat
+  GROUP BY base_id
+),
+UnavailableEndedChats AS (
+  SELECT *
+  FROM chat
+  JOIN MaxChats ON id = maxId
+  AND ended IS NOT null
+),
+MaxMessages AS (
+  SELECT MAX(id) maxId
+  FROM message
+  GROUP BY chat_base_id
+),
+MessagesUpdateTime AS (
+  SELECT 
+    updated,
+    chat_base_id,
+    LOWER(event) AS event_lowercase,
+    (
+      CASE
+        WHEN event = '' THEN NULL
+        ELSE LOWER(event)
+      END
+    ) AS last_message_event
+  FROM message
+  JOIN MaxMessages ON id = maxId
+),
+MessageWithContent AS (
+  SELECT 
+    MAX(id) AS maxId,
+    MIN(id) AS minId
+  FROM message 
+  WHERE content <> ''
+  AND content <> 'message-read'
+  GROUP BY chat_base_id
+),
+FirstContentMessage AS (
+  SELECT created, chat_base_id
+  FROM message
+  JOIN MessageWithContent ON message.id = MessageWithContent.minId
+),
+LastContentMessage AS (
+  SELECT content, chat_base_id
+  FROM message
+  JOIN MessageWithContent ON message.id = MessageWithContent.maxId
+)
 SELECT c.base_id AS id,
        c.customer_support_id,
        c.customer_support_display_name,
-       (CASE WHEN (SELECT value FROM configuration WHERE key = 'is_csa_title_visible' AND configuration.id IN (SELECT max(id) from configuration GROUP BY key) AND deleted = false) = 'true'
-                 THEN c.csa_title ELSE '' END) AS csa_title,
+       (CASE WHEN TitleVisibility.value = 'true' THEN c.csa_title ELSE '' END) AS csa_title,
        c.end_user_id,
        c.end_user_first_name,
        c.end_user_last_name,
@@ -11,38 +77,30 @@ SELECT c.base_id AS id,
        c.end_user_os,
        c.end_user_url,
        c.status,
-       first_message.created,
+       FirstContentMessage.created,
        c.updated,
        c.ended,
        c.forwarded_to_name,
        c.received_from,
        c.labels,
        s.comment,
-       last_content_message.content AS last_message,
-       (CASE WHEN m.event = '' THEN NULL ELSE LOWER(m.event) END) as last_message_event,
-       (SELECT content
-        FROM message
-        WHERE id IN (
-            (SELECT MAX(id) FROM message WHERE event = 'contact-information-fulfilled' AND chat_base_id = c.base_id))) AS contacts_message,
-       m.updated AS last_message_timestamp
-FROM (SELECT * FROM chat WHERE id IN (SELECT MAX(id) FROM chat GROUP BY base_id) AND ended IS NOT null) AS c
-  JOIN (SELECT * FROM message WHERE id IN (SELECT MAX(id) FROM message GROUP BY chat_base_id)) AS m
-  ON c.base_id = m.chat_base_id
-  LEFT JOIN (SELECT chat_id, comment FROM chat_history_comments) AS s
-  ON s.chat_id =  m.chat_base_id
-  JOIN (SELECT * FROM message WHERE id IN (SELECT MAX(id) FROM message 
-          WHERE content <> ''
-          AND content <> 'message-read' GROUP BY chat_base_id)) AS last_content_message
-  ON c.base_id = last_content_message.chat_base_id
-  JOIN (SELECT * FROM message WHERE id IN (SELECT MIN(id) FROM message 
-          WHERE content <> ''
-          AND content <> 'message-read' GROUP BY chat_base_id)) AS first_message
-  ON c.base_id = first_message.chat_base_id
+       LastContentMessage.content AS last_message,
+       MessagesUpdateTime.last_message_event AS last_message_event,
+       ContactsMessage.content AS contacts_message,
+       MessagesUpdateTime.updated AS last_message_timestamp
+FROM UnavailableEndedChats AS c
+LEFT JOIN MessagesUpdateTime ON c.base_id = MessagesUpdateTime.chat_base_id
+LEFT JOIN chat_history_comments AS s ON s.chat_id = MessagesUpdateTime.chat_base_id
+LEFT JOIN LastContentMessage ON c.base_id = LastContentMessage.chat_base_id
+LEFT JOIN FirstContentMessage ON c.base_id = FirstContentMessage.chat_base_id
+LEFT JOIN ContactsMessage ON ContactsMessage.chat_base_id = c.base_id
+CROSS JOIN TitleVisibility
 WHERE c.created::date BETWEEN :start::date AND :end::date
-AND LOWER(m.event) IN (
+AND MessagesUpdateTime.event_lowercase IN (
         'unavailable_holiday',
         'unavailable-contact-information-fulfilled',
         'contact-information-skipped',
         'unavailable_organization',
         'unavailable_csas')
-ORDER BY created;
+ORDER BY created ASC
+LIMIT :limit;
