7e9d9e95a218b3149f24d300a3b637f63db96588
Sander Sepp <sander.sepp@netgroup.com>
2024-08-15 13:45:52 +0300

    feat(#883): Add chat count conf to menu
    - Menu count conf connects the amounts of chats to relevant menu link via path defined in REACT_APP_MENU_JSON. Currently, menu items with paths /unanswered, /active or /pending are supported.

--- GUI/src/components/Layout/index.tsx
+++ GUI/src/components/Layout/index.tsx
@@ -1,21 +1,25 @@
 import React, { FC } from 'react';
 import useStore from 'store';
+import { Header } from '@buerokratt-ria/header';
 import { Outlet } from 'react-router-dom';
 import { MainNavigation } from '@buerokratt-ria/menu';
-import { Header } from '@buerokratt-ria/header';
 import { useToast } from '../../hooks/useToast';
 import './Layout.scss';
+import { useMenuCountConf } from '../../hooks/useMenuCountConf';
 
-const Layout: FC = () => (
-  <div className="layout">
-    <MainNavigation />
-    <div className="layout__wrapper">
-      <Header toastContext={useToast()} user={useStore.getState().userInfo} />
-      <main className="layout__main">
-        <Outlet />
-      </main>
+const Layout: FC = () => {
+  const menuCountConf = useMenuCountConf();
+  return (
+    <div className="layout">
+      <MainNavigation countConf={menuCountConf} />
+      <div className="layout__wrapper">
+        <Header toastContext={useToast()} user={useStore.getState().userInfo} />
+        <main className="layout__main">
+          <Outlet />
+        </main>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Layout;
