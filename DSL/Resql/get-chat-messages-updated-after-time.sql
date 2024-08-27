fa89ab90246b9e37486b8ba412f0eef61863dfea
juri b <juri.butelko@netgroup.com>
2024-08-27 12:53:51 +0300

    Fixes in chat history (#711)  - fixed queries to avoid duplicate users with same id-code

--- DSL/Resql/get-chat-messages-updated-after-time.sql
+++ DSL/Resql/get-chat-messages-updated-after-time.sql
@@ -13,6 +13,17 @@ MaxMessages AS (
 	FROM message
 	WHERE chat_base_id = :chatId
 	GROUP BY base_id
+),
+LatestActiveUser AS (
+  SELECT
+    u.id_code, u.created, u.csa_title
+  FROM
+    "user" u INNER JOIN (
+        SELECT iu.id_code, max(created) AS MaxCreated
+        FROM "user" iu
+        WHERE iu.status = 'active'
+        GROUP BY iu.id_code
+    ) iju ON iju.id_code = u.id_code AND iju.MaxCreated = u.created
 )
 SELECT m.base_id AS id,
        m.chat_base_id AS chatId,
@@ -35,7 +46,7 @@ SELECT m.base_id AS id,
        u.csa_title 
 FROM message m
 LEFT JOIN MessagePreviews mp ON m.chat_base_id = mp.chat_base_id
-LEFT JOIN "user" u ON m.author_id = u.id_code
+LEFT JOIN LatestActiveUser u ON m.author_id = u.id_code
 JOIN MaxMessages ON m.id = maxId
 WHERE :timeRangeBegin::timestamp with time zone < m.updated
 ORDER BY m.created;
--- DSL/Resql/get-chat-messages.sql
+++ DSL/Resql/get-chat-messages.sql
@@ -3,6 +3,17 @@ WITH MaxMessages AS (
   FROM message
   WHERE chat_base_id = :chatId
   GROUP BY base_id
+),
+LatestActiveUser AS (
+  SELECT
+    u.id_code, u.created, u.csa_title
+  FROM
+    "user" u INNER JOIN (
+        SELECT iu.id_code, max(created) AS MaxCreated
+        FROM "user" iu
+        WHERE iu.status = 'active'
+        GROUP BY iu.id_code
+    ) iju ON iju.id_code = u.id_code AND iju.MaxCreated = u.created
 )
 SELECT m.base_id      AS id,
        m.chat_base_id AS chat_id,
@@ -23,6 +34,6 @@ SELECT m.base_id      AS id,
        updated,
        u.csa_title
 FROM message m
-LEFT JOIN "user" u ON m.author_id = u.id_code
+LEFT JOIN LatestActiveUser u ON m.author_id = u.id_code
 JOIN MaxMessages ON m.id = maxId
 ORDER BY created ASC;
