d972a8276f725759604998eebf0909bb5a8cecf1
1AhmedYasser <26207361+1AhmedYasser@users.noreply.github.com>
2024-11-06 21:38:13 +0200

    Added existing checks

--- DSL/Liquibase/changelog/20240621171002-remove_message_content_limit.sql
+++ DSL/Liquibase/changelog/20240621171002-remove_message_content_limit.sql
@@ -1,6 +1,6 @@
 -- liquibase formatted sql
 -- changeset 1AhmedYasser:20240621171002
 
-DROP INDEX idx_message_content;
+DROP INDEX IF EXISTS idx_message_content;
 ALTER TABLE message ALTER COLUMN content TYPE TEXT;
-CREATE INDEX idx_message_content_hash ON message USING btree (MD5(content));
+CREATE INDEX IF NOT EXISTS idx_message_content_hash ON message USING btree (MD5(content));
