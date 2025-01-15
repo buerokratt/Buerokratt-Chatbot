3d9792665c04da8bf0af36fca3b0f55b5068ac8d
Janno Stern <janno@janno.ee>
2025-01-15 12:54:53 +0200

    Remove unintentionally commited changes

--- DSL/Liquibase/changelog/1731915047_add_request_nonces.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE request_nonces(
-        nonce varchar(36) DEFAULT (gen_random_uuid()),
-        created_at TIMESTAMP WITH TIME ZONE DEFAULT (now()),
-        valid_until TIMESTAMP WITH TIME ZONE,
-        used_at TIMESTAMP );
