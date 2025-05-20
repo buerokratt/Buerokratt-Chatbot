0af4509797f493be1c4abb62b8b55e5907f03695
ahmer-mt <ahmer.ali@mindtitan.com>
2025-05-21 02:21:08 +0500

    SCEHMA-004: chat history comments table Indexing and optimization

---
+++ DSL/Liquibase/changelog/20250521021652-rollback.sql
@@ -0,0 +1,8 @@
+-- liquibase formatted sql
+-- changeset ahmer-mt:20250521021652 ignore:true
+DROP INDEX IF EXISTS idx_chat_history_comments_chat_id_created;
+DROP INDEX IF EXISTS idx_chat_history_comments_comment_trigram;
+
+-- Note: Dropping the pg_trgm extension might affect other tables/indexes
+-- Only uncomment this if you're sure no other objects depend on it
+-- DROP EXTENSION IF EXISTS pg_trgm;
