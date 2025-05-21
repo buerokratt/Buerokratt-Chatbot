a030c18f0f25feb6b7915eaa82a1a1ea407a9028
ahmer-mt <ahmer.ali@mindtitan.com>
2025-05-21 12:30:44 +0500

    SCEHMA-004: establishment table Indexing and optimization

--- DSL/Resql/backoffice/GET/org/list_establishments.sql
+++ DSL/Resql/backoffice/GET/org/list_establishments.sql
@@ -1,6 +1,6 @@
 SELECT ARRAY_AGG(name) AS names
-FROM establishment
-WHERE id IN (
-    SELECT MAX(id) FROM establishment
-    GROUP BY base_id
+FROM establishment AS e1
+WHERE created = (
+    SELECT MAX(e2.created) FROM establishment AS e2
+    WHERE e1.base_id = e2.base_id
 ) AND deleted = false
