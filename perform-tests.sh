e07fc6cdee59d1442cbb131058833f45998de144
Artsiom Beida <artsiom.beida@mindtitan.com>
2025-05-21 14:26:47 +0200

    eject test data insertion from changelog

--- perform-tests.sh
+++ perform-tests.sh
@@ -3,7 +3,9 @@ docker compose -f testing-docker-compose.yml up -d
 
 docker run --platform linux/amd64 --network=bykstack riaee/byk-users-db:liquibase20220615 --url=jdbc:postgresql://users_db_test:5432/byk --username=byk --password=01234 --changelog-file=./master.yml update
 
-docker run --rm --network bykstack -v `pwd`/DSL/Liquibase/changelog:/liquibase/changelog -v `pwd`/DSL/Liquibase/master.yml:/liquibase/master.yml -v `pwd`/DSL/Liquibase/data:/liquibase/data liquibase/liquibase --defaultsFile=/liquibase/changelog/liquibase.properties --changelog-file=master.yml --url=jdbc:postgresql://users_db_test:5432/byk?user=byk --password=01234 --contexts=test update
+docker run --rm --network bykstack -v `pwd`/DSL/Liquibase/changelog:/liquibase/changelog -v `pwd`/DSL/Liquibase/master.yml:/liquibase/master.yml -v `pwd`/DSL/Liquibase/data:/liquibase/data liquibase/liquibase --defaultsFile=/liquibase/changelog/liquibase.properties --changelog-file=master.yml --url=jdbc:postgresql://users_db_test:5432/byk?user=byk --password=01234 update
+
+docker run --rm --network bykstack -v `pwd`/DSL/Liquibase/test_data_changelog:/liquibase/test_data_changelog -v `pwd`/DSL/Liquibase/test.yml:/liquibase/test.yml -v `pwd`/DSL/Liquibase/data:/liquibase/data liquibase/liquibase --defaultsFile=/liquibase/changelog/liquibase.properties --changelog-file=test.yml --url=jdbc:postgresql://users_db_test:5432/byk?user=byk --password=01234 --contexts=test update
 
 docker compose -f testing-docker-compose.yml run --rm tests pytest
 
