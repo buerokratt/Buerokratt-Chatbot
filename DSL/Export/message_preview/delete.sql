7d4eac137087c739896859141f605aed84eb3678
Artsiom Beida <artsiom.beida@mindtitan.com>
2025-05-25 22:16:14 +0200

    implemented MIG-002 for message_preview table

---
+++ DSL/Export/message_preview/delete.sql
@@ -0,0 +1,6 @@
+DELETE FROM message_preview
+WHERE (chat_base_id, created) NOT IN (
+    SELECT chat_base_id, max(created)
+    FROM message_preview
+    GROUP BY chat_base_id
+) AND created < %(export_boundary)s;
