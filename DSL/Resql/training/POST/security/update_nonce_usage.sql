491f10c9ea01cc8ccd081b5c4fe01e9198dd40d6
Artsiom Beida <artsiom.beida@mindtitan.com>
2025-05-26 17:46:06 +0200

    added SECURITY-001 declarations for training resql sql files

--- DSL/Resql/training/POST/security/update_nonce_usage.sql
+++ DSL/Resql/training/POST/security/update_nonce_usage.sql
@@ -1,3 +1,22 @@
+/*
+declaration:
+  version: 0.1
+  description: "Mark a request nonce as used by setting the used_at timestamp"
+  method: post
+  accepts: json
+  returns: json
+  namespace: security
+  allowlist:
+    body:
+      - field: updated_nonce
+        type: string
+        description: "The nonce value to mark as used"
+  response:
+    fields:
+      - field: nonce
+        type: string
+        description: "The nonce that was marked as used"
+*/
 SELECT copy_row_with_modifications(
     'request_nonces',
     'nonce', '', nonce,
