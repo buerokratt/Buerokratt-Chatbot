d0423aea9f7601b3420407237cef2326dd9fd686
Artsiom Beida <artsiom.beida@mindtitan.com>
2025-05-26 16:52:08 +0200

    added SECURITY-001 declarations for POST/auth_users sql files

--- DSL/Resql/backoffice/POST/auth_users/create_customer_support_agent_activity.sql
+++ DSL/Resql/backoffice/POST/auth_users/create_customer_support_agent_activity.sql
@@ -1,3 +1,27 @@
+/*
+declaration:
+  version: 0.1
+  description: "Update status and status comment of user by id code"
+  method: post
+  accepts: json
+  returns: json
+  namespace: auth_users
+  allowlist:
+    body:
+      - field: status
+        type: string
+        enum: ['online', 'idle', 'offline']
+        description: "Body field 'status'"
+      - field: statusComment
+        type: string
+        description: "Body field 'statusComment'"
+      - field: active
+        type: boolean
+        description: "Body field 'active'"
+      - field: userIdCode
+        type: string
+        description: "User id code"
+*/
 SELECT copy_row_with_modifications(
     'denormalized_user_data',
    'id', '::UUID', id::VARCHAR,
