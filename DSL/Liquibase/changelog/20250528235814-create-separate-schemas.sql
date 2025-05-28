24e4bc2b6c0f4ce141c026a0ef0a4c1123bbab69
athar-mt <athar.ajmal@mindtitan.com>
2025-05-29 01:49:53 +0500

    Updated migration to move indexes

--- DSL/Liquibase/changelog/20250528235814-create-separate-schemas.sql
+++ DSL/Liquibase/changelog/20250528235814-create-separate-schemas.sql
@@ -7,6 +7,7 @@ CREATE SCHEMA IF NOT EXISTS chat;
 CREATE SCHEMA IF NOT EXISTS config;
 CREATE SCHEMA IF NOT EXISTS org;
 CREATE SCHEMA IF NOT EXISTS security;
+CREATE SCHEMA IF NOT EXISTS analytics;
 
 -- 2. Move tables to their respective schemas
 -- auth_users schema
@@ -30,3 +31,57 @@ ALTER TABLE IF EXISTS public.establishment SET SCHEMA org;
 
 -- security schema
 ALTER TABLE IF EXISTS public.request_nonces SET SCHEMA security;
+
+-- analytics schema
+ALTER TABLE IF EXISTS public.scheduled_reports SET SCHEMA analytics;
+
+DO $$
+DECLARE
+    idx_record RECORD;
+    config_tables TEXT[] := ARRAY['allowed_statuses', 'authority', 'configuration'];
+    chat_tables TEXT[] := ARRAY['chat', 'chat_history_comments', 'chat_jira_syncrhonization', 
+                               'chat_smax_syncrhonization', 'denormalized_chat', 
+                               'denormalized_chat_messages_for_metrics', 'message', 'message_preview'];
+    auth_users_tables TEXT[] := ARRAY['denormalized_user_data', 'user', 
+                                     'user_overview_metric_preference', 'user_page_preference'];
+    org_tables TEXT[] := ARRAY['establishment'];
+    security_tables TEXT[] := ARRAY['request_nonces'];
+    analytics_tables TEXT[] := ARRAY['scheduled_reports'];
+    target_schema TEXT;
+BEGIN
+    FOR idx_record IN 
+        SELECT 
+            t.relname AS table_name,
+            indexrelid::regclass AS idx_name,
+            idx_ns.nspname AS current_schema
+        FROM pg_index i
+        JOIN pg_class t ON i.indrelid = t.oid
+        JOIN pg_namespace n ON t.relnamespace = n.oid
+        JOIN pg_class idx ON i.indexrelid = idx.oid
+        JOIN pg_namespace idx_ns ON idx.relnamespace = idx_ns.oid
+        WHERE t.relname = ANY(config_tables || chat_tables || auth_users_tables || org_tables || security_tables || analytics_tables)
+        AND n.nspname = 'public'  -- Current schema of the tables
+    LOOP
+        -- Determine target schema based on which array the table is in
+        IF idx_record.table_name = ANY(config_tables) THEN
+            target_schema := 'config';
+        ELSIF idx_record.table_name = ANY(chat_tables) THEN
+            target_schema := 'chat';
+        ELSIF idx_record.table_name = ANY(auth_users_tables) THEN
+            target_schema := 'auth_users';
+        ELSIF idx_record.table_name = ANY(org_tables) THEN
+            target_schema := 'org';
+        ELSIF idx_record.table_name = ANY(security_tables) THEN
+            target_schema := 'security';
+        ELSIF idx_record.table_name = ANY(analytics_tables) THEN
+            target_schema := 'analytics';
+        ELSE
+            target_schema := NULL; -- Should not happen with our WHERE clause
+        END IF;
+
+        IF target_schema IS NULL THEN
+            RAISE EXCEPTION 'No target schema found for table %', idx_record.table_name;
+        END IF;
+        EXECUTE format('ALTER INDEX %s SET SCHEMA %I', idx_record.idx_name, target_schema);
+    END LOOP;
+END $$;
