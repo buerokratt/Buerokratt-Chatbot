78f96a04be5c2e0ce0879d1f15a8578ed6d384b3
ahmer-mt <ahmer.ali@mindtitan.com>
2025-05-31 19:14:23 +0500

    fix migrations

--- DSL/Liquibase/changelog/20250528010229-create-scheduled-reports-table-indexes.sql
+++ DSL/Liquibase/changelog/20250528010229-create-scheduled-reports-table-indexes.sql
@@ -1,4 +1,4 @@
 -- liquibase formatted sql
 -- changeset ahmer-mt:20250528010229 ignore:true
 CREATE INDEX idx_scheduled_reports_dataset_updated_deleted
-ON analytics.scheduled_reports (dataset_id, updated DESC, deleted);
+ON scheduled_reports (dataset_id, updated DESC, deleted);
