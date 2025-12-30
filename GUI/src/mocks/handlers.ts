5af107da0f57a064392d84a3188f9af389b9712d
Igor Krupenja <igor.krupenja@gmail.com>
2025-12-30 08:46:09 +0200

    feat(1663): wip mock

--- GUI/src/mocks/handlers.ts
+++ GUI/src/mocks/handlers.ts
@@ -1,23 +1,12 @@
 import { rest } from 'msw';
 
 import { establishmentsData } from './establishments';
-import { healthzStatusData } from './healthzStatus';
-import { usersData } from './users';
 import { EstablishmentsResponse } from '../types/establishment';
 
 const baseUrl = import.meta.env.REACT_APP_RUUTER_PRIVATE_API_URL;
 
 export const handlers = [
-  rest.get(baseUrl + 'accounts/admins', (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(usersData));
-  }),
-  rest.get(baseUrl + 'health/components-status', (req, res, ctx) => {
-    return res(ctx.json(healthzStatusData));
-  }),
-  rest.post(baseUrl + 'attachments/add', (req, res, ctx) => {
-    return res(ctx.status(200));
-  }),
-  rest.get(baseUrl + 'configs/centops-establishments', (req, res, ctx) => {
+  rest.get(baseUrl + '/configs/centops-establishments', (req, res, ctx) => {
     const page = Number(req.url.searchParams.get('page')) || 1;
     const pageSize = Number(req.url.searchParams.get('pageSize')) || 10;
 
