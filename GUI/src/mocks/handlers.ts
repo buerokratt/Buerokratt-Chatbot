1992d234f1c0cad7d26cdca022c21c014ca1a87b
Igor Krupenja <igor.krupenja@gmail.com>
2025-12-30 07:36:37 +0200

    feat(1663): non working mock

--- GUI/src/mocks/handlers.ts
+++ GUI/src/mocks/handlers.ts
@@ -1,7 +1,9 @@
 import { rest } from 'msw';
 
+import { establishmentsData } from './establishments';
 import { healthzStatusData } from './healthzStatus';
 import { usersData } from './users';
+import { EstablishmentsResponse } from '../types/establishment';
 
 const BASE_URL = import.meta.env.BASE_URL;
 
@@ -14,5 +16,27 @@ export const handlers = [
   }),
   rest.post(BASE_URL + 'attachments/add', (req, res, ctx) => {
     return res(ctx.status(200));
+  }),
+  rest.get(BASE_URL + 'configs/centops-establishments', (req, res, ctx) => {
+    const page = Number(req.url.searchParams.get('page')) || 1;
+    const pageSize = Number(req.url.searchParams.get('pageSize')) || 10;
+
+    console.log('WE GOOD BRO');
+
+    const startIndex = (page - 1) * pageSize;
+    const endIndex = startIndex + pageSize;
+    const paginatedItems = establishmentsData.slice(startIndex, endIndex);
+    const totalPages = Math.ceil(establishmentsData.length / pageSize);
+
+    const response: EstablishmentsResponse = {
+      response: {
+        items: paginatedItems,
+        page,
+        pageSize,
+        totalPages,
+      },
+    };
+
+    return res(ctx.status(200), ctx.json(response));
   }),
 ];
