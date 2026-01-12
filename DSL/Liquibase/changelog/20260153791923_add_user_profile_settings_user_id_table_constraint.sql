a9152408251c3b6419fadded5d07edf3af01ed7b
ffrose <119657383+ffrose@users.noreply.github.com>
2026-01-12 13:18:09 +0200

    Update and rename 20260153791922_add_user_profile_settings_user_id_table_constraint.sql to 20260153791923_add_user_profile_settings_user_id_table_constraint.sql

--- DSL/Liquibase/changelog/20260153791923_add_user_profile_settings_user_id_table_constraint.sql
+++ DSL/Liquibase/changelog/20260153791923_add_user_profile_settings_user_id_table_constraint.sql
@@ -3,5 +3,12 @@
 ALTER TABLE user_profile_settings 
 DROP CONSTRAINT IF EXISTS user_profile_settings_pkey;
 
+DELETE FROM user_profile_settings
+WHERE id NOT IN (
+    SELECT MAX(id)
+    FROM user_profile_settings
+    GROUP BY user_id
+);
+
 ALTER TABLE user_profile_settings 
 ADD CONSTRAINT user_profile_settings_user_id_key UNIQUE (user_id);
