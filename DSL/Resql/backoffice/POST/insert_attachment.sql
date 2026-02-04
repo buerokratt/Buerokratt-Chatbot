5326282ba7a05cb882024ae350a82b43d52b6d3c
Igor Krupenja <igor.krupenja@gmail.com>
2026-02-04 17:06:10 +0200

    wip dl

--- DSL/Resql/backoffice/POST/insert_attachment.sql
+++ DSL/Resql/backoffice/POST/insert_attachment.sql
@@ -1,2 +1,2 @@
 INSERT INTO attachment(base_id, chat_base_id, message_base_id, file_name, file_size, mime_type, storage_path, uploaded_by, uploaded_by_role, status, created)
-VALUES (:baseId, :chatBaseId, NULLIF(:messageBaseId, ''), :fileName, :fileSize::bigint, :mimeType, :storagePath, NULLIF(:uploadedBy, ''), :uploadedByRole, 'available', NOW());
+VALUES (:baseId, :chatBaseId, NULLIF(:messageBaseId, ''), :fileName, :fileSize::bigint, :mimeType, :storagePath, COALESCE(NULLIF(:uploadedBy, ''), 'anonymous'), :uploadedByRole, 'available', NOW());
