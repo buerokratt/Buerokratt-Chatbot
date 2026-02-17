b6e8aa6a2b9864cc65bcec91ae11982494f2b004
1AhmedYasser <26207361+1AhmedYasser@users.noreply.github.com>
2026-02-17 22:32:14 +0200

    fix(1799): Fixed format

--- GUI/src/services/users.ts
+++ GUI/src/services/users.ts
@@ -15,7 +15,8 @@ export async function createUser(userData: UserDTO) {
     csa_email: userData.csaEmail,
     roles: authorities.length === 0 ? Object.values(userData.authorities) : authorities,
     department: userData.department,
-    domains: userData.domains.length === 0 || userData.domains[0] === null ? [] : userData.domains.map((d) => d.value ?? d),
+    domains:
+      userData.domains.length === 0 || userData.domains[0] === null ? [] : userData.domains.map((d) => d.value ?? d),
   });
   return data;
 }
@@ -45,7 +46,8 @@ export async function editUser(id: string | number, userData: UserDTO, smaxConne
     jiraAccountId: userData.jiraAccountId,
     roles: authorities.length === 0 ? Object.values(userData.authorities) : authorities,
     department: userData.department,
-    domains: userData.domains.length === 0 || userData.domains[0] === null ? [] : userData.domains.map((d) => d.value ?? d),
+    domains:
+      userData.domains.length === 0 || userData.domains[0] === null ? [] : userData.domains.map((d) => d.value ?? d),
   });
   return data;
 }
