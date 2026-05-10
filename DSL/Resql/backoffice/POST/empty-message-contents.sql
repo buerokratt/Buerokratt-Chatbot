522bb7259916898a19e3c1a5c9beb88b9ab52732
1AhmedYasser <26207361+1AhmedYasser@users.noreply.github.com>
2026-05-10 11:47:48 +0300

    chore(855): Delete end user info when deleting chats

--- DSL/Resql/backoffice/POST/empty-message-contents.sql
+++ DSL/Resql/backoffice/POST/empty-message-contents.sql
@@ -1,3 +1,11 @@
 UPDATE message
 SET content = ''
 WHERE chat_base_id IN (:chats);
+
+UPDATE chat
+SET end_user_id         = NULL,
+    end_user_first_name = NULL,
+    end_user_last_name  = NULL,
+    end_user_email      = NULL,
+    end_user_phone      = NULL
+WHERE base_id IN (:chats);
