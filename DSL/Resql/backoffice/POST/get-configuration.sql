c279b80c96a45b9dbf5e3284f74fdfa7e3d7fed9
1AhmedYasser <26207361+1AhmedYasser@users.noreply.github.com>
2026-05-26 12:21:08 +0300

    chore(1975): Added get-domain-configuration

--- DSL/Resql/backoffice/POST/get-configuration.sql
+++ DSL/Resql/backoffice/POST/get-configuration.sql
@@ -1,17 +1,5 @@
 SELECT id, key, value
 FROM configuration
-WHERE key = :key
-  AND NOT deleted
-  AND (
-    :domain IS NULL
-    OR "domain" = :domain::UUID
-  )
-  AND id IN (
-    SELECT max(id)
-    FROM configuration
-    WHERE (
-      :domain IS NULL
-      OR "domain" = :domain::UUID
-    )
-    GROUP BY key
-  );
+WHERE key=:key
+AND id IN (SELECT max(id) from configuration GROUP BY key)
+AND NOT deleted;
