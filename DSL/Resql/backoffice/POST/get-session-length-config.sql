ba028a8b4314a36efb45940a15f8f6a973ab79c8
1AhmedYasser <26207361+1AhmedYasser@users.noreply.github.com>
2026-09-03 04:15:34 +0300

    chore(1770): Modified away status functionality

--- DSL/Resql/backoffice/POST/get-session-length-config.sql
+++ DSL/Resql/backoffice/POST/get-session-length-config.sql
@@ -6,7 +6,9 @@ WHERE key IN (
     'show_idle_warning',
     'idle_message',
     'show_auto_close_text',
-    'auto_close_text'
+    'auto_close_text',
+    'away_status_active',
+    'away_status_timeout'
     )
   AND id IN (SELECT max(id) from configuration GROUP BY key)
   AND NOT deleted;
